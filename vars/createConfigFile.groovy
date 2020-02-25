ea0e7a73946113192d381462f32256ec299c8473
carlosrodlop <it.carlosrodlop@gmail.com>
2020-02-25 19:46:08 +0100

    passing yaml

--- vars/createConfigFile.groovy
+++ vars/createConfigFile.groovy
@@ -1,4 +1,7 @@
 // vars/createConfigFile.groovy
+// Requires 
+// java.io.File java.lang.String
+// staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods leftShift java.io.File java.lang.Object
 def call(yaml) {
    def pipelineConfig = new File('pipelineConfig.yaml')
    pipelineConfig << "${yaml}" 
