38d58eedfa7d97227a2ee9fe1751c90c32cdcc25
carlosrodlop <it.carlosrodlop@gmail.com>
2020-02-26 13:12:06 +0100

    multibranch typo

--- vars/mavenK8Project.groovy
+++ vars/mavenK8Project.groovy
@@ -19,7 +19,7 @@ def call(configYaml) {
             DOCKER_DESTINATION = "${config.docker_registry}/${config.docker_image}:${config.docker_tag}"
         }
         stages {
-            stage ("Skip CD/CI for protected branch") {
+            stage ("Multibranch: Skip CD/CI for protected branch") {
                 when {
                     branch "${PROTECTED_BRANCH}"
                 }
