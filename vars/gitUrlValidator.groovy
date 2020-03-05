bba99a3c669081d067594f0121f1fff6c3b1247a
carlosrodlop <it.carlosrodlop@gmail.com>
2020-03-05 19:21:50 +0100

    trim url

--- vars/gitUrlValidator.groovy
+++ vars/gitUrlValidator.groovy
@@ -1,6 +1,6 @@
 // vars/gitUrlValidator.groovy
 def call(url) {
-  if (! url.startsWith("https") || ! url.endsWith (".git")){
+  if (! url.trim().startsWith("https") || ! url.trim().endsWith (".git")){
     error("The git url must start with 'https' and ends with '.git'. Example: https://github.com/example-org/example-repo.git")
   }
 }
