2b42e03aef808ec0802cd15c661b2a70a5c0ea6f
carlosrodlop <it.carlosrodlop@gmail.com>
2020-03-22 19:22:21 +0100

    typos and indentation

--- vars/mavenK8Project.groovy
+++ vars/mavenK8Project.groovy
@@ -18,18 +18,18 @@ def call(configYaml) {
             DOCKERFILE_PATH = "${config.d_path.trim()}"
         }
         stages {
-            stage ("Skip build check?") {          
+            stage ("Skip Run?") {          
                 when {
-                    changeset "**/template.yaml"
+                    anyOf { changeset "**/template.yaml"; changeset "**/Jenkinsfile"} 
                 }
                 steps {
-                    echo "Aborting build because template.yaml is within the changetset files"
+                    echo "Aborting Pipeline due to changes are coming from Template definition"
                     script{
                         currentBuild.result = 'aborted'
                     }
                 }
             }
-            stage ("Run Pipeline") {
+            stage ("Run") {
                 agent {
                     kubernetes {
                         defaultContainer "maven"
@@ -43,60 +43,60 @@ def call(configYaml) {
                             sh "cat config.yaml"
                         }
                     }
-                stage("Checkout") {
-                    environment {
-                        GIT_PARAM_CREDENTIALS = "${config.g_cred}"
-                        GIT_PARAM_REPO = "${config.g_repo.trim()}"
-                        DOCKER_IMAGE_LATEST = "${config.d_latest}"
-                    }
-                    steps {
-                        script {
-                            if (GIT_PARAM_BRANCH == "") {
-                                echo "Pipeline Multibranch detected"
-                                checkout([$class: 'GitSCM', branches: [[name: "${BRANCH_NAME}"]], userRemoteConfigs: [[credentialsId: "${GIT_PARAM_CREDENTIALS}", url: "${GIT_PARAM_REPO}"]]])
-                                git_currentBranch = "${BRANCH_NAME}"
-                            } else {
-                                echo "Pipeline non Multibranch detected"
-                                git branch: "${GIT_PARAM_BRANCH}", credentialsId: "${GIT_PARAM_CREDENTIALS}" , url: "${GIT_PARAM_REPO}"
-                                git_currentBranch = "${GIT_PARAM_BRANCH}"
-                            }
-                            git_repo = sh(script: "basename '${GIT_PARAM_REPO}' .git", returnStdout: true).trim()
-                            if (DOCKER_IMAGE_LATEST == "false") {
-                                echo "Tagging image with commit"
-                                git_commit = sh(script: "git rev-parse --short=5 ${GIT_COMMIT}", returnStdout: true).trim()
-                            } else {
-                                echo "Tagging image as latest"
-                                git_commit = "latest"
+                    stage("Checkout") {
+                        environment {
+                            GIT_PARAM_CREDENTIALS = "${config.g_cred}"
+                            GIT_PARAM_REPO = "${config.g_repo.trim()}"
+                            DOCKER_IMAGE_LATEST = "${config.d_latest}"
+                        }
+                        steps {
+                            script {
+                                if (GIT_PARAM_BRANCH == "") {
+                                    echo "Pipeline Multibranch detected"
+                                    checkout([$class: 'GitSCM', branches: [[name: "${BRANCH_NAME}"]], userRemoteConfigs: [[credentialsId: "${GIT_PARAM_CREDENTIALS}", url: "${GIT_PARAM_REPO}"]]])
+                                    git_currentBranch = "${BRANCH_NAME}"
+                                } else {
+                                    echo "Pipeline non Multibranch detected"
+                                    git branch: "${GIT_PARAM_BRANCH}", credentialsId: "${GIT_PARAM_CREDENTIALS}" , url: "${GIT_PARAM_REPO}"
+                                    git_currentBranch = "${GIT_PARAM_BRANCH}"
+                                }
+                                git_repo = sh(script: "basename '${GIT_PARAM_REPO}' .git", returnStdout: true).trim()
+                                if (DOCKER_IMAGE_LATEST == "false") {
+                                    echo "Tagging image with commit"
+                                    git_commit = sh(script: "git rev-parse --short=5 ${GIT_COMMIT}", returnStdout: true).trim()
+                                } else {
+                                    echo "Tagging image as latest"
+                                    git_commit = "latest"
+                                }
                             }
                         }
                     }
-                }
-                stage("Build") {
-                    steps {
-                        sh "mvn clean package -Dmaven.test.skip=true"
-                        archiveArtifacts artifacts: "config.yaml, target/*.jar", fingerprint: true
-                        stash name: "docker", includes: "config.yaml, target/*.jar, ${DOCKERFILE_PATH}"
+                    stage("Build") {
+                        steps {
+                            sh "mvn clean package -Dmaven.test.skip=true"
+                            archiveArtifacts artifacts: "config.yaml, target/*.jar", fingerprint: true
+                            stash name: "docker", includes: "config.yaml, target/*.jar, ${DOCKERFILE_PATH}"
+                        }
                     }
-                }
-                stage("Test") {
-                    steps {
-                        sh 'mvn clean test'
-                        junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+                    stage("Test") {
+                        steps {
+                            sh 'mvn clean test'
+                            junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+                        }
                     }
-                }
-                stage("Publish in Registry") {
-                    environment {
-                        DOCKER_DESTINATION = "${config.d_registry.trim()}/${git_repo}_${git_currentBranch}:${git_commit}"
-                    }
-                    steps {
-                        container(name: "kaniko", shell: "/busybox/sh") {
-                            dir ("unstash"){ // To avoid java.nio.file.AccessDeniedException ... example-app.jar
-                                unstash "docker"
-                                sh "/kaniko/executor --dockerfile `pwd`/${DOCKERFILE_PATH} --context `pwd` --destination ${DOCKER_DESTINATION}"
+                    stage("Publish in Registry") {
+                        environment {
+                            DOCKER_DESTINATION = "${config.d_registry.trim()}/${git_repo}_${git_currentBranch}:${git_commit}"
+                        }
+                        steps {
+                            container(name: "kaniko", shell: "/busybox/sh") {
+                                dir ("unstash"){ // To avoid java.nio.file.AccessDeniedException ... example-app.jar
+                                    unstash "docker"
+                                    sh "/kaniko/executor --dockerfile `pwd`/${DOCKERFILE_PATH} --context `pwd` --destination ${DOCKER_DESTINATION}"
+                                }
                             }
                         }
                     }
-                }
             }
         }
     }
